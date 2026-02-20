64333e830833af4801a757b092d47d1f09d1c9d8
Divine Ifediorah <d-ifediorah@leadway.com>
2026-02-20 20:56:54 +0100

    filing modal

--- apps/backend/src/clients/client.ts
+++ apps/backend/src/clients/client.ts
@@ -11,7 +11,7 @@ declare global {
 }
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: process.env.VITE_API_URL || 'http://localhost:3001',
   withCredentials: true,
 });
 
